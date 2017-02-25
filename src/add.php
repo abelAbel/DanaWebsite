537026f4a234703ef4c6d5745c051ecb0fe66784
abelamadou <aamadou@msudenver.edu>
2017-02-25 09:15:22 -0700

    Initial commit

---
+++ src/add.php
@@ -0,0 +1,30 @@
+
+<?php 
+
+	$pdo = new PDO();
+	$title= $_POST['title'];
+	
+		// $searchInput = $_GET['query'];
+		// echo 'hello ' . $searchInput;
+
+		// echo 
+		// "
+		// <div data-role='page' id='bar'>
+
+		// 	<div data-role='header'>
+		// 		<h1>Bar</h1>
+		// 	</div><!-- /header -->
+
+		// <div role='main' class='ui-content'>
+		// 	<p>I'm the second in the source order so I'm hidden when the page loads. I'm just shown if a link that references my id is beeing clicked.</p>
+		// 	<p><a href='#foo'>Back to foo</a></p>
+		// </div><!-- /content -->
+
+		// <div data-role='footer'>
+		// 	<h4>Page Footer</h4>
+		// </div><!-- /footer -->
+		// </div><!-- /page -->
+
+
+		// "
+?>
---
+++ src/jquery_mobile.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+    var loaded = false;
+
+
+
+// Add Page
+    $("#p1").on( "swiperight", function( event ) {
+        // alert("swiperight");
+        $( ":mobile-pagecontainer" ).pagecontainer( "change", "#pAdd", { transition: "slide", reverse: true} );
+        // window.open("http://www.google.com", "_system");
+    });
+
+    $("#pAdd").on( "swipeleft", function( event ) {
+        // alert("swiperight");
+        $( ":mobile-pagecontainer" ).pagecontainer( "change", "#p1", { transition: "slide"} );
+        // window.open("http://www.google.com", "_system");
+    });
+
+    $("#googleButton").on("tap",function(){
+        window.open("http://www.google.com", "_system");
+    });
+
+
+
+
+// Result Page
+    $("#p1").on( "swipeleft", function( event ) {
+        // alert("swipeleft");
+        if (loaded == false) {
+            alert("Please search for something");
+        }
+        else{
+            // alert("Page is loaded");
+            $( ":mobile-pagecontainer" ).pagecontainer( "change", "#pResults", { transition: "slide" } );
+
+        }
+    });
+
+    $("#pResults").on( "swiperight", function( event ) {
+        $( ":mobile-pagecontainer" ).pagecontainer( "change", "#p1", { transition: "slide", reverse: true} );
+    });
+
+    $("#slider-fill").on("change",function (e) {
+            
+        $("#p1").css("background","red");
+    });
+
+
+
+
+
+
+    $("form.ajax").on("submit",function (event) {
+
+        var url = $(this).attr('action'),
+            type = $(this).attr('method'),
+            data = {};
+
+        $(this).find('[name]').each(function (index, value) {
+            var name = $(this).attr('name'),
+                value = $(this).val();
+                data[name] = value;
+
+        });
+        console.log(data);
+
+        $.ajax({
+            url:url,
+            type:type,
+            data: data,
+            success: function  (response) {
+                loaded = true;
+                // body...
+            }
+        });
+
+        
+        return false;
+    });
+
+});
+
+
